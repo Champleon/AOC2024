a2db174ef43b31481dc80df4419701a9bc76051c
Champleon <77229088+Champleon@users.noreply.github.com>
2024-12-01 09:56:52 +0100

    Initial commit

---
+++ Advent01.java
@@ -0,0 +1,22 @@
+public class Advent01 
+{
+    public static void main(String[] args) 
+    {
+        
+    }
+
+    public int distances(int[] list1, int[] list2)
+    {
+        return 0;
+    }
+
+    public int[] mergeSort(int[] x)
+    {
+        int n = x.length;
+        int[] sortedList = new int[n];
+
+
+
+        return sortedList;
+    }
+}
